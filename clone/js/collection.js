61d9f9b5cb594b0b4882d571d33e715f430de00f
Sovidi <133857264+Sovidi@users.noreply.github.com>
2023-07-19 18:18:05 +0900

    1251252
    12125

---
+++ clone/js/collection.js
@@ -0,0 +1,82 @@
+
+
+function colClick(){
+    let colStorageNum =  localStorage.getItem("num");
+
+    fetch('./json/data.json')
+    .then(res => { return res.json()}) // 한줄일땐 (),{]생략가능 근데 {}쓸땐 return시켜줘야함 안쓰면 필없
+    .then(data => {
+
+        // 메인 소장유물 클릭시 유물 타이틀 정보 불러오기
+        const collTitleBox = document.querySelector('.collection-title');
+        collTitleBox.innerHTML = `
+        <div class="left">
+            <h4 class="title">${data.items[colStorageNum].name}</h4>
+        </div>
+
+        <div class="right">
+            <ul>
+                <li>
+                    <p>유형</p>
+                    <span>${data.items[colStorageNum].era}</span>
+                </li>
+                <li>
+                    <p>시대분류</p>
+                    <span>${data.items[colStorageNum].type}</span>
+                </li>
+                <li>
+                    <p>작가(출처)</p>
+                    <span>${data.items[colStorageNum].author}</span>
+                </li>
+                <li>
+                    <p>크기</p>
+                    <span>${data.items[colStorageNum].size}</span>
+                </li>
+                <li>
+                    <p>제작연도</p>
+                    <span>${data.items[colStorageNum].year}</span>
+                </li>
+                <li>
+                    <p>소품번호</p>
+                    <span>${data.items[colStorageNum].colNum}</span>
+                </li>
+            </ul>
+        </div>
+        `;
+
+        //  메인 소장유물 클릭시 유물 이미지 정보 불러오기
+        const collImgBox = document.querySelector('.img-box');
+
+        collImgBox.innerHTML = `
+                <div class="img-print">
+                <img src="./${data.items[colStorageNum].img}" alt="">
+                </div>
+        `;
+
+        const elSpan = document.querySelectorAll('.right > ul > li > span'),
+                elLi = document.querySelectorAll('.right > ul > li');
+
+        elSpan.forEach(function(v,k){
+            if(elSpan[k].childNodes.length == 0){
+                elLi[k].style = 'display : none;'
+            }
+        })
+    })
+
+
+}
+
+colClick();
+
+
+
+
+var swiper = new Swiper(".dif-coll", {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 50,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
--- clone/js/custom.js
+++ clone/js/custom.js
@@ -1,3 +1,5 @@
+
+// 박물관 새소식 스와이퍼
 var swiper = new Swiper('.first-swiper', {
   loop: true,
   slidesPerView: 3,
@@ -16,6 +18,8 @@ var swiper = new Swiper('.first-swiper', {
 
 })
 
+
+// 공지사항 스와이퍼
 var swiper = new Swiper(".mySwiper", { 
       direction: "vertical",
       centeredSlides: true,
@@ -24,47 +28,89 @@ var swiper = new Swiper(".mySwiper", {
         delay: 1500,
         disableOnInteraction: false,
       },
-    });
-
-
-
-window.addEventListener('load',()=>{
-
-  
-    $('.grid').masonry({
-      itemSelector: '.grid-item',
-      columnWidth: $('.grid-item:first').width(),
-      percentPosition: true,
-      gutter: '.gutter-sizer',
       
     });
-  
-});
 
-/*
-function maso(){
-  $('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: $('.grid-item:first').width(),
-    percentPosition: true,
-    gutter: '.gutter-sizer',
-    
-  });
-}
-window.addEventListener('resize',maso);
-*/
-
-
-
+// 박물관 새소식 백그라운드 이미지 스크롤에따라 위치변동
 const bgImg = document.querySelector('.deco-only-pc');
 
-
 function bgScroll(){
-  let y = pageYOffset/4;
+  let y = pageYOffset/4; //scrollY
   bgImg.style = `left:${y}px`
 }
-
 
 window.addEventListener('scroll',bgScroll);
 
 
+//소장유물 json데이터 가져오기
+
+  const collImg = document.querySelector('.collections-img > img'),
+    colType = document.querySelector('.info > .category'),
+    colTitle = document.querySelector('.info > .collections-title');
+
+
+    fetch('./json/data.json')
+    .then(res => { return res.json()}) // 한줄일땐 (),{]생략가능 근데 {}쓸땐 return시켜줘야함 안쓰면 필없
+    .then(data => {
+
+        data.items.forEach((obj,k) => {
+          colBox = document.querySelector('.collections-content');
+            colBox.innerHTML += `
+            <li class="grid-item">
+            <a href="./collections-click.html">
+                <div class="collections-img">
+                    <img src="${obj.img}" alt="z">
+                    <div class="screen">
+                        <span>
+                            자세히 알아보기
+                        </span>
+                    </div>
+                </div>
+                <div class="info">
+                    <p class="category">
+                        ${obj.type}
+                    </p>
+                    <p class="collections-title">
+                        ${obj.name}
+                    </p>
+                </div>
+            </a>
+        </li>
+            `;
+        })
+
+        //이미지들 변수로 잡아주고
+        const elGridItem  = document.querySelectorAll('.grid-item img');
+        // 이미지 반복문 돌려서 다 나오게한뒤 로드이벤트 주기
+        elGridItem.forEach((eleImg,key)=>{
+          eleImg.addEventListener('load',()=>{
+            // 계속 반복문실행중일꺼니 이미지 전체 길이값과 foreach에 키값 비교해서 같으면 이제 그만실행
+            if(elGridItem.length-1 == key) maso(); 
+          })
+        })
+
+        const itemLink = document.querySelectorAll('.grid-item > a');
+
+        itemLink.forEach(function(v,k){
+          v.onclick = function(e){
+            localStorage.setItem("num",k)
+          }
+        })
+      
+    })
+
+
+
+
+        // 소장유물 masonry
+        function maso(){
+          $('.grid').masonry({
+            itemSelector: '.grid-item',
+            columnWidth: $('.grid-item:first').width(),
+            percentPosition: true,
+            gutter: '.gutter-sizer',
+            
+          });
+      
+        }
+    
