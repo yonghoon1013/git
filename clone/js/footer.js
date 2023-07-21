5a5001d44c82526d46765ac3b575fb4baebd7072
Sovidi <133857448+yonghoon1013@users.noreply.github.com>
2023-07-21 12:22:55 +0900

    dffdssa
    fsdafd

--- clone/js/footer.js
+++ clone/js/footer.js
@@ -1,8 +1,19 @@
 const topBtn = document.querySelector('.scroll-top');
 function scroll(){
+
   window.scrollTo({ top: 0, behavior: 'smooth'});  
 }
 
+function scroll2(){
+  
+  let y = pageYOffset; //scrollY
+  if(y < 160){
+    topBtn.classList.add('none');
+  } else{
+    topBtn.classList.remove('none');
+  }
+
+}
+
 topBtn.addEventListener('click',scroll);
-
-
+window.addEventListener('scroll',scroll2);
